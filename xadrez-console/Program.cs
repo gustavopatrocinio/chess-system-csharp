fd00084767cd90a3318eb38fda51cf62d5d064fb
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2021-02-01 19:48:02 -0300

    Starting to implement a method that prints the board

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,6 +9,8 @@ namespace xadrez_console
         {
             Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tabuleiro); 
+
             Console.ReadLine();
 
         }
