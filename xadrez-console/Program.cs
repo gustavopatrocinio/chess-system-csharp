842546be54868e4ccc76ecad3270f957aab321d5
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2021-02-03 11:18:28 -0300

    Improving the printing of pieces on the board

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,13 +8,23 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
 
-            Console.WriteLine(pos.toPosicao());
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(3, 5));
 
 
+                Tela.imprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException e)
+            { 
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
 
         }
